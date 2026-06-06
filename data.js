d1b6b95338d13cdd6b021038a4f96cfb29e8a32e
PyaeSonePaing-104 <peterpaingcoding@gmail.com>
2026-06-07 03:41:53 +0630

    add data in data.js file

--- data.js
+++ data.js
@@ -1,3 +1,26 @@
 const data = [
-    { heading :}
+    { heading : 'ထီးယူသွားပါ☔️' ,
+      text : 'Make sure to grab an umbrella before you head out' ,
+      image : 'images/rain.gif'
+    } ,
+    { heading : 'အိမ်သော့မေ့မယ် သဲသဲ🔑' ,
+      text : 'Remember to take your keys with you' ,
+      image : 'images/key.gif'
+    } ,
+    { heading : 'အနွေးထည်ကောပါရဲ့လား 🧥' ,
+      text : 'Are you sure you have your jacket with you?' ,
+      image : 'images/cold.gif'
+    } ,
+    { heading : 'လမ်းကူးရင်သေချာကူးပါ🚸' ,
+      text : 'Be sure to check for traffic when you are crossing the street' ,
+      image : 'images/road.gif'
+    } ,
+    { heading : 'Boltကိုဖြေးဖြေးမောင်းခိုင်းပါ🛵' ,
+      text : 'Remember to tell the driver to go slowly when you take a Bolt motorbike' ,
+      image : 'images/drive.gif'
+    } ,
+    { heading : 'Suncream မမေ့နဲ့နော်🧴' ,
+      text : 'Make sure you pack some sunscreen' ,
+      image : 'images/sun.gif'
+    } ,
 ]
