95a5e33a8b4f4dccdf0975ab43a9279b1dcae762
PyaeSonePaing-104 <peterpaingcoding@gmail.com>
2026-06-08 00:07:38 +0630

    render function for change inner html

--- images/index.js
+++ images/index.js
@@ -1,1 +1,11 @@
 import { data } from './data.js'
+
+
+
+
+function render (){
+    const randomOutput = Math.floor(Math.random()* data.length)
+    document.getElementById('heading').innerHTML = data[randomOutput].heading
+    document.getElementById('paragraph').innerHTML = data[randomOutput].text
+    document.getElementsByClassName('main-image').innerHTML=data[randomOutput].image
+}
