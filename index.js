683094d966b0d58271f62dad3c8b760e769f7656
PyaeSonePaing-104 <peterpaingcoding@gmail.com>
2026-06-08 01:45:14 +0630

    Final JS

--- index.js
+++ index.js
@@ -1,32 +1,43 @@
 import { data } from './data.js'
 
- document.querySelector('.btn').addEventListener('click', randomMessage)
- document.querySelector('.fa-power-off').addEventListener('click', restart)
+
+const messageBtn = document.querySelector('.btn')
+const powerBtn = document.querySelector('.fa-power-off')
+const heading = document.getElementById('heading')
+const paragraph = document.getElementById('paragraph')
+const mainImage = document.querySelector('.main-image')
 
 let isClicked = false
 
-function render (){
-    const randomOutput = Math.floor(Math.random()* data.length)
-    document.getElementById('heading').innerHTML = data[randomOutput].heading
-    document.getElementById('paragraph').innerHTML = data[randomOutput].text
-    document.querySelector('.main-image').src=data[randomOutput].image
+
+messageBtn.addEventListener('click', randomMessage)
+powerBtn.addEventListener('click', restart)
+
+
+function render() {
+    const randomOutput = Math.floor(Math.random() * data.length)
+    
+    
+    heading.innerHTML = data[randomOutput].heading
+    paragraph.innerHTML = data[randomOutput].text
+    mainImage.src = data[randomOutput].image
 }
 
-function randomMessage (){
-      setTimeout (function(){
+function randomMessage() {
+    setTimeout(function() {
         isClicked = true
-        document.querySelector('.fa-power-off').style.display ='inline-block'
-        return render ()
-      },500)
+        powerBtn.style.display = 'inline-block'
+        render() 
+    }, 500)
 }
 
-function restart(){
-       
-      if(isClicked){
-      document.getElementById('heading').innerHTML ='သဲသဲလေးရေ!'
-       document.getElementById('paragraph').innerHTML = 'If koko knocked out or busy and you need to slip out, just leave a quick tap here!'
-       document.querySelector('.main-image').src='./images/main-image.png'
-       isClicked = false 
-       document.querySelector('.fa-power-off').style.display ='none'
-       
-} }     
+function restart() {
+    if (isClicked) {
+        heading.innerHTML = 'သဲသဲလေးရေ!'
+        paragraph.innerHTML = 'If koko knocked out or busy and you need to slip out, just leave a quick tap here!'
+        mainImage.src = './images/main-image.png'
+        
+        isClicked = false 
+        powerBtn.style.display = 'none'
+    }
+}
